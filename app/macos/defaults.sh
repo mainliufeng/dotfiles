d6d427fbffb360d59ec62344b9ccdd5a68b5a7fa
刘峰 <mainliufeng@gmail.com>
2016-08-27 11:15:12 +0800

    show hidden files in finder and not on desktop

--- app/macos/defaults.sh
+++ app/macos/defaults.sh
@@ -1,3 +1,7 @@
 # Dock
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock orientation -string 'left'
+
+defaults write com.apple.finder AppleShowAllFiles TRUE
+defaults write com.apple.finder CreateDesktop -bool false
+killall Finder
