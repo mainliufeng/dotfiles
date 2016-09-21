7287b89985b50ec8e18e9d64d3077fa901766450
刘峰 <mainliufeng@gmail.com>
2016-09-21 15:01:01 +0800

    defaults

--- app/macos/defaults.sh
+++ app/macos/defaults.sh
@@ -2,6 +2,21 @@
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock orientation -string 'left'
 
+# Show all files in finder and not in desktop
 defaults write com.apple.finder AppleShowAllFiles TRUE
 defaults write com.apple.finder CreateDesktop -bool false
 killall Finder
+
+# Enable tap to click (Trackpad) for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
