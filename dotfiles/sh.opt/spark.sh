3cffdcd8b69fafed80174a0d64b9edd81bb3490f
刘峰 <mainliufeng@gmail.com>
2016-10-09 09:59:52 +0800

    brew SPARK_HOME is in libexec

--- dotfiles/sh.opt/spark.sh
+++ dotfiles/sh.opt/spark.sh
@@ -1,5 +1,5 @@
 ## spark
-export SPARK_HOME=/usr/local/opt/apache-spark
+export SPARK_HOME=/usr/local/opt/apache-spark/libexec
 export PYTHONPATH=$SPARK_HOME/python/:$PYTHONPATH
 
 alias spark_debug_on='export SPARK_JAVA_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5555'
