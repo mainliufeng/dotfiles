4c0c4a3c16af3a75aebea8fdd85664e9242978a4
刘峰 <mainliufeng@gmail.com>
2016-11-12 11:28:33 +0800

    maven completion

--- dotfiles/zsh/oh-my-zsh.zsh
+++ dotfiles/zsh/oh-my-zsh.zsh
@@ -49,7 +49,7 @@ ZSH_THEME="cobalt2"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git python fasd virtualenv virtualenvwrapper extract sublime docker kubectl)
+plugins=(git python fasd virtualenv virtualenvwrapper extract sublime docker kubectl mvn)
 autoload -U compinit && compinit
 
 source $ZSH/oh-my-zsh.sh
