56541f56d897fdc35c22050251cad7ec80a74065
刘峰 <mainliufeng@gmail.com>
2016-12-02 09:07:36 +0800

    unset PYTHONPATH

--- dotfiles/sh.opt/spark.sh
+++ dotfiles/sh.opt/spark.sh
@@ -1,6 +1,6 @@
 ## spark
 export SPARK_HOME=/usr/local/opt/apache-spark/libexec
-export PYTHONPATH=$SPARK_HOME/python/:$PYTHONPATH
+#export PYTHONPATH=$SPARK_HOME/python/:$PYTHONPATH
 
 alias spark_debug_on='export SPARK_JAVA_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5555'
 alias spark_debug_off='unset SPARK_JAVA_OPTS'
