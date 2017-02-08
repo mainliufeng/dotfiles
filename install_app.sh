687ccb159dc9217c99f4be8a77a0ceb01cfd4f14
刘峰 <mainliufeng@gmail.com>
2017-02-08 09:27:07 +0800

    spacemacs

--- install_app.sh
+++ install_app.sh
@@ -23,3 +23,10 @@ sudo softwareupdate -i -a &> $LOG
 
 echo "set macos defaults"
 sh ~/dotfiles/macos/defaults.sh &> $LOG
+
+echo "install spacemacs"
+if [ -d "$HOME/.emacs.d" ]; then
+    mv ~/.emacs.d ~/.emacs.d.bak
+    mv ~/.emacs ~/.emacs.bak
+fi
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
