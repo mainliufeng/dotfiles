f65513cc986fb8764bd180b1994c4640bdad9bed
刘峰 <mainliufeng@gmail.com>
2017-02-13 11:01:05 +0800

    remove mou from brewfile only install base apps with travis build

---
+++ travis_install_app.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ "$(uname -s)" != "Darwin" ]; then
+    echo "support only macos"
+    exit 1
+fi
+
+LOG=./install.log
+
+if ! type brew > /dev/null; then
+    echo "install homebrew"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &> $LOG
+else
+    echo "skip homebrew brew command exist"
+fi
+
+echo "install brew apps"
+brew bundle --file ~/dotfiles/brewfiles/Brewfile.base &> $LOG
+sudo softwareupdate -i -a &> $LOG
+
+echo "set macos defaults"
+sh ~/dotfiles/macos/defaults.sh &> $LOG
+
+echo "install spacemacs"
+if [ -d "$HOME/.emacs.d" ]; then
+    mv ~/.emacs.d ~/.emacs.d.bak
+    mv ~/.emacs ~/.emacs.bak
+fi
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
