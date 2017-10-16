41b7a262eea00d848ce4050f1e99934d3e80ec64
刘峰 <mainliufeng@gmail.com>
2017-10-16 11:43:31 +0800

    change tmux config

--- sh/function/ssh.sh
+++ sh/function/ssh.sh
@@ -1,3 +1,3 @@
-ssh() {
-    /usr/bin/ssh -t "$@" "tmux attach -t liufeng || tmux new -s liufeng"
-}
+#ssh() {
+#    /usr/bin/ssh -t "$@" "tmux attach -t liufeng || tmux new -s liufeng; PS1='[\[\e[33;40m\]\u@\h \w]$ '; exec bash"
+#}
