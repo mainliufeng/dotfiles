d8c1be7203dcd6584d4f035a509d6511629bd081
刘峰 <mainliufeng@gmail.com>
2020-02-26 20:31:06 +0800

    install nodejs (required by coc.vim)

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,2 +1,6 @@
+#!/bin/sh
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 pip3 install --user pynvim
+
+# install nodejs (required by coc.vim)
+brew install node
