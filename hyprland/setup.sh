2cf170a59f2f26339cdd4f7cb250c105336423df
刘峰 <mainliufeng@gmail.com>
2023-06-02 14:03:59 +0800

    hyprland clipboard manager

--- hyprland/setup.sh
+++ hyprland/setup.sh
@@ -1,1 +1,1 @@
-yay -S hyprland-hidpi-xprop-git waybar-hyprland hyprpaper cliphist wofi swaylock-effects swayidle xdg-desktop-portal-hyprland grim slurp grimblast-git gimp hyprprop guake
+yay -S hyprland-hidpi-xprop-git waybar-hyprland hyprpaper cliphist wofi swaylock-effects swayidle xdg-desktop-portal-hyprland grim slurp grimblast-git gimp hyprprop guake cliphist
