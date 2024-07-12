479afa2467bba44157b58ef58ebbddc5fe09f805
Liu Feng <mainliufeng@gmail.com>
2024-07-12 22:36:59 +0800

    update hyprland config, add plasma6 key bindings

--- hyprland/setup.sh
+++ hyprland/setup.sh
@@ -1,1 +1,1 @@
-yay -S hyprland waybar-hyprland-git hyprpaper cliphist wofi swaylock-effects swayidle xdg-desktop-portal-hyprland grim slurp grimblast-git gimp hyprprop cliphist wireplumber hyprcursor
+yay -S hyprland waybar hyprpaper cliphist wofi swaylock-effects swayidle xdg-desktop-portal-hyprland grim slurp grimblast-git gimp hyprprop cliphist wireplumber hyprcursor
