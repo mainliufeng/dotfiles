2a6c2b8cd7e3b865a4a22f560d20c9fa0847a956
Liu Feng <mainliufeng@gmail.com>
2024-10-30 12:03:21 +0800

    vscode nvim config

--- nvim/lua/mainliufengvscode/init.lua
+++ nvim/lua/mainliufengvscode/init.lua
@@ -34,3 +34,24 @@ require("lazy").setup({
     },
 })
 
+local function cmd(command)
+    return table.concat({ '<Cmd>', command, '<CR>' })
+end
+
+-- 快捷键
+local opts = { silent = true, noremap = true }
+local function keymap(m, k, c)
+    return vim.keymap.set(m, k, c, opts)
+end
+
+-- 跳-word
+keymap("n", "s", cmd "HopWord")
+
+keymap("n", ";", ":call VSCodeNotify('workbench.action.showCommands')<CR>")
+
+keymap("n", "gi", ":call VSCodeNotify('editor.action.goToImplementation')<CR>")
+keymap("n", "gr", ":call VSCodeNotify('editor.action.goToReferences')<CR>")
+keymap("n", "gD", ":call VSCodeNotify('editor.action.goToTypeDefinition')<CR>")
+keymap("n", "gd", ":call VSCodeNotify('editor.action.goToDeclaration')<CR>")
+keymap("n", "<space>rn", ":call VSCodeNotify('editor.action.rename')<CR>")
+
