74e1c13aa4b51a735fba0805ea103a216cbcb9e2
Liu Feng <mainliufeng@gmail.com>
2024-11-01 12:12:00 +0800

    openapi diff

---
+++ go/setup_openapi_diff.sh
@@ -0,0 +1,4 @@
+go install github.com/tufin/oasdiff@latest
+
+# 使用
+# oasdiff changelog https://raw.githubusercontent.com/Tufin/oasdiff/main/data/openapi-test1.yaml https://raw.githubusercontent.com/Tufin/oasdiff/main/data/openapi-test5.yaml
