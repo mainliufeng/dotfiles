c9d936837ac5479050b07a371910d570e2542c29
Liu Feng <mainliufeng@gmail.com>
2025-06-21 08:30:53 +0800

    picom

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -634,5 +634,4 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 -- }}}
 
 awful.spawn.once("/usr/lib/org_kde_powerdevil")
-awful.spawn.with_shell("pgrep volumeicon || volumeicon &")
-awful.spawn.with_shell("pgrep picom || picom --config ~/.config/awesome/picom.conf --vsync")
+awful.spawn.with_shell("picom --config ~/.config/awesome/picom.conf --vsync &")
