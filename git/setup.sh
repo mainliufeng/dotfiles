a8cc5feba37f8ada405bd2f646bfb25f528de8c4
Liu Feng <mainliufeng@gmail.com>
2025-11-13 16:50:58 +0800

    配置git时间格式

--- git/setup.sh
+++ git/setup.sh
@@ -1,5 +1,6 @@
 sudo pacman -S git git-lfs github-cli
 git config --global user.email "mainliufeng@gmail.com"
 git config --global user.name "Liu Feng"
+git config --global log.date "format:%Y-%m-%d %H:%M:%S"
 
 # 安装github-cli后执行gh auth login
