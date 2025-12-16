74cf2fbe9ebead124ce77b2052fdb0b396c2c569
Liu Feng <mainliufeng@gmail.com>
2025-12-16 17:33:34 +0800

    Fix Chrome scaling on KDE/Xorg

--- chrome/link.sh
+++ chrome/link.sh
@@ -1,2 +1,5 @@
 mkdir -p ~/.config
 ln -svfn ~/dotfiles/chrome/chrome-flags.conf ~/.config/chrome-flags.conf
+
+mkdir -p ~/.local/share/applications
+ln -svfn ~/dotfiles/chrome/applications/google-chrome.desktop ~/.local/share/applications/google-chrome.desktop
