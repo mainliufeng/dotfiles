4d858ca53525daa91e77e4d0eb398308dae59f16
Liu Feng <mainliufeng@gmail.com>
2026-01-15 22:01:27 +0800

    update alma config

--- alma/link.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p ~/.local/share/applications
-ln -svfn ~/dotfiles/alma/applications/alma.desktop ~/.local/share/applications/alma.desktop
-rm -f "${XDG_CACHE_HOME:-$HOME/.cache}/hypr-launcher/apps.tsv"
