9c0ad1c95933337316a1d90566b4dce802e805fb
Liu Feng <mainliufeng@gmail.com>
2026-01-19 21:57:02 +0800

    update codex config

--- codex/link.sh
+++ codex/link.sh
@@ -1,8 +1,8 @@
 mkdir -p ~/.codex
 ln -svfn ~/dotfiles/codex/config.toml ~/.codex/config.toml
-ln -svfn ~/dotfiles/codex/prompts ~/.codex/prompts
+#ln -svfn ~/dotfiles/codex/prompts ~/.codex/prompts
 mkdir -p ~/.codex/skills
-rsync -a ~/dotfiles/codex/skills/ ~/.codex/skills/
+#rsync -a ~/dotfiles/codex/skills/ ~/.codex/skills/
 
 superpowers_dir="$HOME/.codex/superpowers"
 if [ -d "$superpowers_dir" ]; then
@@ -15,21 +15,3 @@ if [ -d "$superpowers_dir" ]; then
 else
   git clone https://github.com/obra/superpowers.git "$superpowers_dir"
 fi
-
-bash ~/dotfiles/codex/agents_md/install.sh
-node ~/dotfiles/codex/agents_md/agents-md.js enable base superpowers
-
-python3 ~/.codex/skills/.system/skill-installer/scripts/install-skill-from-github.py \
-  --repo anthropics/skills \
-  --path skills/webapp-testing
-
-python3 ~/.codex/skills/.system/skill-installer/scripts/install-skill-from-github.py \
-  --repo anthropics/skills \
-  --path skills/frontend-design
-
-python3 ~/.codex/skills/.system/skill-installer/scripts/install-skill-from-github.py \
-  --repo michalparkola/tapestry-skills-for-claude-code \
-  --path tapestry
-
-python3 ~/.codex/skills/.system/skill-installer/scripts/install-skill-from-github.py \
-  --url https://github.com/ComposioHQ/awesome-claude-skills/tree/master/content-research-writer
